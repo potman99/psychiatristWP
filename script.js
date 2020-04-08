ae1b304a60be9c3f86448575dd46aa442dd79fa5
Jan Potocki <potmanv8@gmail.com>
2020-04-08 02:22:09 +0200

    start of burger menu

---
+++ script.js
@@ -0,0 +1,44 @@
+const mapDOM = document.querySelector('#map');
+const moreBtn = document.querySelector('#btnMore');
+const directBtn = document.querySelector('#directBtn');
+
+const posLink = "https://www.google.com/maps/place/Poczta+Polska/@50.947845,21.373203,17z/data=!4m5!3m4!1s0x0:0xf6a85261b2a6a2a0!8m2!3d50.9479652!4d21.372462?hl=pl-PL";
+
+
+//////////////////////
+// Google Maps
+
+function initMap(){
+
+    var pos = {lat: 50.947788, lng: 21.372892};
+
+    var map = new google.maps.Map(
+        mapDOM,
+        {zoom:18,center:pos}
+    );
+
+    var contentString = `<h2><a href=${posLink}>Sienkiewicza 80,<br> 27-400, Ostrowiec Świętokrzyski</a></h2>`
+
+    var info = new google.maps.InfoWindow({
+        content:contentString
+    });
+
+    var marker = new google.maps.Marker({
+        position: pos, map:map
+    });
+
+    info.open(map,marker);
+
+}
+
+///////////////////////////
+/////   Rellax Init
+
+
+let rellax = new Rellax('.rellax');
+
+
+
+directBtn.addEventListener('click',()=>{
+    window.location.replace(posLink);
+});
