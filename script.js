91c75985c6b80147b80b50c7650a62605763afa7
Jan Potocki <potmanv8@gmail.com>
2020-04-08 18:02:26 +0200

    mobile menu + scrollTop

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const mapDOM = document.querySelector('#map');
 const moreBtn = document.querySelector('#btnMore');
 const directBtn = document.querySelector('#directBtn');
+const scrollTopBtn = document.querySelector('.homeBtn');
 
 const posLink = "https://www.google.com/maps/place/Poczta+Polska/@50.947845,21.373203,17z/data=!4m5!3m4!1s0x0:0xf6a85261b2a6a2a0!8m2!3d50.9479652!4d21.372462?hl=pl-PL";
 
@@ -42,3 +43,45 @@ let rellax = new Rellax('.rellax');
 directBtn.addEventListener('click',()=>{
     window.location.replace(posLink);
 });
+
+
+/////////////////////////
+//// Mobile Menu
+
+const menuOpenBtn = document.querySelector(".menuOpen");
+const menuCloseBtn = document.querySelector(".menuClose");
+const navBar = document.querySelector(".navBar");
+
+menuOpenBtn.addEventListener('click',()=>{
+
+    navBar.classList.toggle('navBarActive');
+});
+
+menuCloseBtn.addEventListener('click',()=>{
+    navBar.classList.add('navBarClose');
+    navBar.classList.remove('navBarActive');
+    
+});
+
+menuOpenBtn.addEventListener('click',()=>{
+    navBar.classList.remove('navBarClose');
+    navBar.classList.add('navBarActive');
+    
+});
+
+window.onscroll = ()=>{
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+
+    if(scrolled >30.0){
+        scrollTopBtn.classList.add('homeBtnIn');
+        scrollTopBtn.classList.remove('homeBtnOut');
+    }
+    else{
+        scrollTopBtn.classList.remove('homeBtnIn');
+        scrollTopBtn.classList.add('homeBtnOut');
+
+    }
+    
+}
